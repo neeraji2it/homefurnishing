c52203c71e58a38aac2e2a1001ebc3d25f3672eb
Ranjana <rockingrails@gmail.com>
2015-06-18 17:33:12 +0530

    image update feature for collection third

--- app/models/collection_third.rb
+++ app/models/collection_third.rb
@@ -1,2 +1,4 @@
 class CollectionThird < ActiveRecord::Base
+  has_attached_file :image, :styles => { :thumb => "700x460", :small => "80x80"}, :default_url => "/assets/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
