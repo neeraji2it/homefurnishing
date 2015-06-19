8ae749764b0420da1c02413ee7e19afbef8fc038
Ranjana <rockingrails@gmail.com>
2015-06-19 18:52:27 +0530

    Attachment thumb created

--- app/models/collection_seventh.rb
+++ app/models/collection_seventh.rb
@@ -1,4 +1,4 @@
 class CollectionSeventh < ActiveRecord::Base
-  has_attached_file :image, :styles => { :thumb => "700x460", :small => "80x80"}, :default_url => "/assets/:style/missing.png"
+  has_attached_file :image, :styles => { :thumb => "315x154"}, :default_url => "/assets/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
