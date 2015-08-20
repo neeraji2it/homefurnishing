85ab490f4efb5cd84a224b70170a54b9539fd843
rails bhagath <rails@gmail1.com>
2015-08-20 18:32:17 +0530

    omniauth credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-   if Rails.env == 'development'
- provider :facebook, '839830756038749', '838ebe25c3cd54225e96190707a4cada', {:scope => 'email,publish_actions,offline_access'}
+    provider :facebook, '885691864836924', '12e5ec4e562cfd2f159f6bec4b6c915a', {:provider_ignores_state => true}
 
-     elsif Rails.env == 'production'
-    provider :facebook, '885691864836924', '12e5ec4e562cfd2f159f6bec4b6c915a',{:scope => 'email,publish_actions,offline_access'}
-  end
 end
-
 
