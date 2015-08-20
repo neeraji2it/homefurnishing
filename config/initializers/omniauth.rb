3ba098771cf6d56eb5fd511fe8cb0839b9517cbe
rails bhagath <rails@gmail1.com>
2015-08-20 16:27:28 +0530

    changed omniauth credentials for rosara site

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, '839830756038749', '838ebe25c3cd54225e96190707a4cada', {:provider_ignores_state => true}
+    provider :facebook, '885691864836924', '12e5ec4e562cfd2f159f6bec4b6c915a', {:provider_ignores_state => true}
 
 end
