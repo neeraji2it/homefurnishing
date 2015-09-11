ff430526b61134893f1e60ec7b62e6574a9f37c5
rails bhagath <rails@gmail1.com>
2015-09-11 14:05:11 +0530

    changed mail in mailer

--- app/mailers/newsletter.rb
+++ app/mailers/newsletter.rb
@@ -2,15 +2,15 @@ class Newsletter < ActionMailer::Base
   default from: "oe.member1@gmail.com"
   def send_contact(news_letter)
     @news_letter = news_letter
-   # mail(:to => "info@rosara.in", :subject => 'Signup for newsletter')
-    mail(:to => "bhagat@dine-media.com", :subject => 'Signup for newsletter')
+   mail(:to => "info@rosara.in", :subject => 'Signup for newsletter')
+   # mail(:to => "bhagat@dine-media.com", :subject => 'Signup for newsletter')
   end
 
 
 def order_info(order)
     @order = order
-   # mail(:to => "info@rosara.in", :subject => 'New Order')
-    mail(:to => "bhagat@dine-media.com", :subject => 'New Order')
+    mail(:to => "info@rosara.in", :subject => 'New Order')
+   # mail(:to => "bhagat@dine-media.com", :subject => 'New Order')
 end
 
 
